86cd1cf66c54dfffb1ad8b1edc293bfa1d50e132
shreya-sridhar <shreyasridhar1109@gmail.com>
2020-11-17 23:13:37 +0000

    Assignment complete

--- newsletter.rb
+++ newsletter.rb
@@ -30,9 +30,10 @@ ARTICLES = [
 def calculate_recipients
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
+  SUBSCRIBERS - UNSUBSCRIBED
 end
 
-def first_n_articles(number_of_articles
+def first_n_articles(number_of_articles)
   ARTICLES.first(number_of_articles)
 end
 
@@ -40,21 +41,29 @@ def print_recipients
   # Write a method that uses the output of calculate_recipients
   # and returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
+  puts calculate_recipients.join(", ")
 end
 
 def print_one_article(article)
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
+    puts("#{article[:title]}")
+    puts("by: #{article[:author]}")
+    puts(article[:text])
 end
 
 def print_many_articles(articles)
   # Write a method that will take in an array of article hashes
   # and format each one using the print_one_article method
+  articles.each do |article_hash|
+    print_one_article(article_hash)
+    puts("\n")
+  end
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  "Flatiron #{campus[:name]}"
 end
 
 def format_subject
@@ -62,10 +71,11 @@ def format_subject
 end
 
 def format_footer(campus)
-  "Flatiron Newsletter · #{campus[:name]} · #{campus[:address]} "
+  puts "Flatiron Newsletter · #{campus[:name]} · #{campus[:address]} "
 end
 
 def print_newsletter(number)
+
   puts "Generating this week's newsletter...\n\n"
 
   print "SUBJECT: "
@@ -76,11 +86,11 @@ def print_newsletter(number)
 
   puts "\nBODY:"
   format_subject
-  articles = first_n_articles(number)
+  
+  articles = first_n_articles(number.to_i)
+
   print_many_articles(articles)
   puts format_footer(CAMPUS)
-
-  end
 end
 
 def run
